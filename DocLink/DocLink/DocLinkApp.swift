9acfc8ead329985c0a728c9372b6166f77de76a7
Adam Mirzakanov <adamirzakan@gmail.com>
2024-11-14 02:13:59 +0300

    build: initialize Xcode project

---
+++ DocLink/DocLink/DocLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocLinkApp.swift
+//  DocLink
+//
+//  Created by Адам Мирзаканов on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
